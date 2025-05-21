8deae6549a60f6a7c9fac4fdb7c04229687d559f
Si6gma <neiv.malaviya@gmail.com>
2025-05-21 14:22:46 +0200

    Unfinished SD V1

--- storage.h
+++ storage.h
@@ -5,6 +5,11 @@
 #include <SPI.h>
 #include <FS.h>
 
+#define SD_MOSI     23
+#define SD_MISO     19
+#define SD_SCLK     18
+#define SD_CS       5
+
 void initSD();
 void write(char *filename, char *data);
 bool fileExists(char *filename);
