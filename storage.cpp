0163fab71d4e9b747410bb5f351103b1818dd216
Si6gma <90695450+Si6gma@users.noreply.github.com>
2025-05-21 15:07:06 +0200

    Added stringData, removed several things

--- storage.cpp
+++ storage.cpp
@@ -6,18 +6,25 @@ void initSD() {
   SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
   if (!SD.begin(SD_CS))
     Serial.println("ERROR IN BEGINNING SD");
-    while (1) {}
+  while (1) {
+  }
   {
-}
+  }
 
-void write(char *filename, char *data) {
+  void write(char *filename, char *data) {
+  }
 
-}
+  bool fileExists(char *filename) {
+    return false;
+  }
 
-bool fileExists(char *filename) { 
-  return false;
-}
+  void renameFile(char *prevFilename, char *newFilename) {
+  }
 
-void renameFile(char *prevFilename, char* newFilename) {
+  String stringData() {
+    return String(gpsData.time, 2) + "," + String(gpsData.latitude, 6) + "," + String(gpsData.longitude, 6) + "," + String(getGForce()) + "," + String(gpsData.satCount);
+  }
 
-}
+  String headerData() {
+    return String("UTC Time,Latitude,Longitude,G-Force,satCount");
+  }
--- storage.h
+++ storage.h
@@ -4,11 +4,12 @@
 #include <SD.h>
 #include <SPI.h>
 #include <FS.h>
+#include "gps.h"
 
-#define SD_MOSI     23
-#define SD_MISO     19
-#define SD_SCLK     18
-#define SD_CS       5
+#define SD_MOSI 23
+#define SD_MISO 19
+#define SD_SCLK 18
+#define SD_CS 5
 
 void initSD();
 void write(char *filename, char *data);
